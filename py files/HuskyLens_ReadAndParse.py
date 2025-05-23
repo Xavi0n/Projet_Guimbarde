74ac31ae5a91061deec60c03b3682d6bcaaa13c6
Xavi0n <xavchampoux@gmail.com>
2025-05-23 13:06:42 -0400

    ajustements au protocole I2C

--- py files/HuskyLens_ReadAndParse.py
+++ py files/HuskyLens_ReadAndParse.py
@@ -3,95 +3,148 @@ from smbus import SMBus
 import time
 
 HUSKYLENS_I2C_ADDR = 0x32  # 7-bit address
-CMD_REQUEST_BLOCKS = [0x55, 0xAA, 0x11, 0x00, 0x11]  # Request all learned blocks
+
+# HuskyLens Commands
+COMMAND_HEADER = 0x55
+COMMAND_END = 0x54
+COMMAND_REQUEST_KNOCK = 0x2C
+COMMAND_REQUEST_ALGORITHM = 0x2D
+COMMAND_REQUEST_BLOCKS = 0x21
+COMMAND_REQUEST_ARROWS = 0x22
+COMMAND_REQUEST_LEARNED = 0x24
+COMMAND_REQUEST_BLOCKS_LEARNED = 0x25  # This is what we'll use for trained objects
+COMMAND_REQUEST_ARROWS_LEARNED = 0x26
+COMMAND_REQUEST_BY_ID = 0x27
+COMMAND_REQUEST_BLOCKS_BY_ID = 0x28
+COMMAND_REQUEST_ARROWS_BY_ID = 0x29
+
+def calculate_checksum(data):
+    return sum(data) & 0xFF
 
 def request_blocks_i2c(I2C_bus_number=1):
     try:
         print("Debug: Opening I2C bus {}".format(I2C_bus_number))
         I2C_bus = SMBus(I2C_bus_number)
         
-        # Send request command
-        print("Debug: Sending command bytes: {}".format([hex(x) for x in CMD_REQUEST_BLOCKS]))
-        I2C_bus.write_i2c_block_data(HUSKYLENS_I2C_ADDR, CMD_REQUEST_BLOCKS[0], CMD_REQUEST_BLOCKS[1:])
-        time.sleep(0.05)
-
-        # Read up to 64 bytes
+        # First, knock to check if HuskyLens is ready
+        knock_command = [COMMAND_HEADER, COMMAND_HEADER, COMMAND_REQUEST_KNOCK, 0x00, 0x00]
+        knock_command.append(calculate_checksum(knock_command[2:4]))
+        
+        print("Debug: Sending knock command: {}".format([hex(x) for x in knock_command]))
+        I2C_bus.write_i2c_block_data(HUSKYLENS_I2C_ADDR, knock_command[0], knock_command[1:])
+        time.sleep(0.1)  # Wait for HuskyLens to process
+        
+        # Try to read knock response
+        try:
+            knock_response = []
+            for _ in range(5):
+                byte = I2C_bus.read_byte(HUSKYLENS_I2C_ADDR)
+                knock_response.append(byte)
+            print("Debug: Knock response: {}".format([hex(x) for x in knock_response]))
+            
+            # Verify knock response
+            if knock_response[0] != COMMAND_HEADER or knock_response[1] != COMMAND_HEADER:
+                print("Debug: Invalid knock response")
+                return None
+                
+        except IOError as e:
+            print("Debug: Knock read error: {}".format(e))
+        
+        # Now send request for learned blocks
+        request_command = [COMMAND_HEADER, COMMAND_HEADER, COMMAND_REQUEST_BLOCKS_LEARNED, 0x00, 0x00]
+        request_command.append(calculate_checksum(request_command[2:4]))
+        
+        print("Debug: Sending learned blocks request: {}".format([hex(x) for x in request_command]))
+        I2C_bus.write_i2c_block_data(HUSKYLENS_I2C_ADDR, request_command[0], request_command[1:])
+        time.sleep(0.2)  # Give more time for processing
+        
+        # Read response
         I2C_response_bytes = []
         try:
-            print("Debug: Reading response bytes...")
-            for _ in range(64):
+            # First read header (should be 5 bytes)
+            header = []
+            for _ in range(5):
                 byte = I2C_bus.read_byte(HUSKYLENS_I2C_ADDR)
-                I2C_response_bytes.append(byte)
-                print("Debug: Read byte: {}".format(hex(byte)))
+                header.append(byte)
+            print("Debug: Response header: {}".format([hex(x) for x in header]))
+            
+            if header[0] == COMMAND_HEADER and header[1] == COMMAND_HEADER:
+                # Valid header found, read the rest based on length
+                length = header[3] + (header[4] << 8)
+                print("Debug: Expected data length: {}".format(length))
+                
+                # Read the data and checksum
+                for _ in range(length + 1):  # +1 for checksum
+                    byte = I2C_bus.read_byte(HUSKYLENS_I2C_ADDR)
+                    I2C_response_bytes.append(byte)
+                    print("Debug: Read data byte: {}".format(hex(byte)))
+            
         except IOError as e:
-            print("Debug: Stopped reading at {} bytes. Reason: {}".format(len(I2C_response_bytes), e))
-            pass  # Stop reading when no more data
-
-        I2C_bus.close()
-        print("Debug: Total bytes read: {}".format(len(I2C_response_bytes)))
-        if I2C_response_bytes:
-            print("Debug: First few bytes: {}".format([hex(x) for x in I2C_response_bytes[:8]]))
-        return I2C_response_bytes
+            print("Debug: Data read error: {}".format(e))
+        finally:
+            I2C_bus.close()
+        
+        if header and I2C_response_bytes:
+            full_response = header + I2C_response_bytes
+            print("Debug: Full response: {}".format([hex(x) for x in full_response]))
+            return full_response
+        return None
+        
     except Exception as e:
         print("I2C error: {}".format(e))
+        if 'I2C_bus' in locals():
+            I2C_bus.close()
         return None
 
-def parse_huskylens_response(I2C_data_bytes):
-    if not I2C_data_bytes:
-        print("Debug: No data bytes to parse")
+def parse_huskylens_response(data):
+    if not data or len(data) < 5:
+        print("Debug: Invalid response data")
         return []
         
-    I2C_FRAME_HEADER = [0x55, 0xAA]
-    I2C_FRAME_TOTAL_SIZE = 16
-    I2C_PAYLOAD_SIZE = 10
-    I2C_parsed_objects = []
-
-    print("Debug: Starting to parse {} bytes".format(len(I2C_data_bytes)))
+    print("Debug: Parsing response of {} bytes".format(len(data)))
+    objects = []
     
-    I2C_index = 0
-    while I2C_index < len(I2C_data_bytes) - I2C_FRAME_TOTAL_SIZE + 1:
-        if I2C_data_bytes[I2C_index] == I2C_FRAME_HEADER[0] and I2C_data_bytes[I2C_index + 1] == I2C_FRAME_HEADER[1]:
-            I2C_frame = I2C_data_bytes[I2C_index:I2C_index + I2C_FRAME_TOTAL_SIZE]
-            print("Debug: Found frame at index {}: {}".format(I2C_index, [hex(x) for x in I2C_frame]))
-
-            I2C_address = I2C_frame[2]
-            I2C_length = I2C_frame[3] + (I2C_frame[4] << 8)
-            if I2C_address != 0x11 or I2C_length != I2C_PAYLOAD_SIZE:
-                print("Debug: Invalid frame - address: {}, length: {}".format(hex(I2C_address), I2C_length))
-                I2C_index += 1
-                continue
-
-            I2C_checksum = I2C_frame[-1]
-            I2C_checksum_calc = (sum(I2C_frame[2:-1])) & 0xFF
-            if I2C_checksum != I2C_checksum_calc:
-                print("Debug: Checksum mismatch - expected: {}, got: {}".format(hex(I2C_checksum), hex(I2C_checksum_calc)))
-                I2C_index += 1
-                continue
-
-            I2C_payload = I2C_frame[5:15]
-            I2C_obj_id    = I2C_payload[0] + (I2C_payload[1] << 8)
-            I2C_x_center  = I2C_payload[2] + (I2C_payload[3] << 8)
-            I2C_y_center  = I2C_payload[4] + (I2C_payload[5] << 8)
-            I2C_width     = I2C_payload[6] + (I2C_payload[7] << 8)
-            I2C_height    = I2C_payload[8] + (I2C_payload[9] << 8)
-
-            print("Debug: Parsed object - ID: {}, pos: ({}, {}), size: {}x{}".format(
-                I2C_obj_id, I2C_x_center, I2C_y_center, I2C_width, I2C_height))
-
-            I2C_parsed_objects.append({
-                'id': I2C_obj_id,
-                'x': I2C_x_center,
-                'y': I2C_y_center,
-                'width': I2C_width,
-                'height': I2C_height
+    # Check header
+    if data[0] != COMMAND_HEADER or data[1] != COMMAND_HEADER:
+        print("Debug: Invalid header")
+        return []
+        
+    # Get length
+    length = data[3] + (data[4] << 8)
+    print("Debug: Data length from header: {}".format(length))
+    
+    # Check if we have enough data
+    if len(data) < length + 6:  # header(5) + data(length) + checksum(1)
+        print("Debug: Incomplete data")
+        return []
+        
+    # Process data blocks
+    data_start = 5
+    while data_start < len(data) - 5:  # Need at least 5 bytes for a block
+        try:
+            obj_id = data[data_start] + (data[data_start + 1] << 8)
+            x = data[data_start + 2] + (data[data_start + 3] << 8)
+            y = data[data_start + 4] + (data[data_start + 5] << 8)
+            width = data[data_start + 6] + (data[data_start + 7] << 8)
+            height = data[data_start + 8] + (data[data_start + 9] << 8)
+            
+            print("Debug: Found object - ID: {}, pos: ({}, {}), size: {}x{}".format(
+                obj_id, x, y, width, height))
+                
+            objects.append({
+                'id': obj_id,
+                'x': x,
+                'y': y,
+                'width': width,
+                'height': height
             })
-
-            I2C_index += I2C_FRAME_TOTAL_SIZE
-        else:
-            I2C_index += 1
-
-    print("Debug: Parsed {} objects".format(len(I2C_parsed_objects)))
-    return I2C_parsed_objects
+            
+            data_start += 10
+        except IndexError:
+            break
+            
+    print("Debug: Parsed {} objects".format(len(objects)))
+    return objects
 
 def get_parsed_huskylens_objects():
     print("Debug: Starting HuskyLens object detection")
