d98c0d111410644c529090a11ad18f9efc4e9fba
Xavi0n <xavchampoux@gmail.com>
2025-05-27 15:54:26 -0400

    developpement de la fonctionnalite de la huskylens

--- py files/HuskyLens_ReadAndParse.py
+++ py files/HuskyLens_ReadAndParse.py
@@ -1,22 +1,15 @@
 from huskylib import HuskyLensLibrary
-import time
 
-def get_huskylens_data():
-    
+def main():
     huskylens = HuskyLensLibrary("I2C", 0x32)
     huskylens.algorithm("ALGORITHM_OBJECT_RECOGNITION")
 
-    try:
-        while True:
-            time.sleep(1)  # Wait for a second before requesting data
-            if (objects := huskylens.requestAll()):
-                print("Data received:", objects)
-                print("Debug: Raw data: {}".format([hex(x) for x in objects]))
-                for obj in objects:
-                    print("Object ID: {obj.id}, X: {obj.x}, Y: {obj.y}, Width: {obj.width}, Height: {obj.height}")
+    objects = huskylens.requestAll()
+    if not objects:
+        return  # No output if no objects
 
-            else:
-                print("No objects found, please try again")
+    for obj in objects:
+        print(f"{obj.x} {obj.y} {obj.width} {obj.height} {obj.ID}")
 
-    except KeyboardInterrupt:
-        print("Stopped")
+if __name__ == "__main__":
+    main()
--- src/huskylens_api.c
+++ src/huskylens_api.c
@@ -1,99 +1,43 @@
-#define PY_SSIZE_T_CLEAN
-#include "huskylens_api.h"
-#include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// This function initializes Python, calls Python function, and fills the objects array.
-// Returns the number of objects parsed, or -1 on error.
-// max_objects: max number of objects to read (avoid overflow)
-// objs: array of HuskylensObject to fill
-int get_huskylens_objects(HuskylensObject *objs, int max_objects) {
-    // Check for NULL pointer
-    if (objs == NULL) {
-        return -1;
+#define MAX_LINE_LEN 128
+#define MAX_BLOCKS 32
+
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+    int ID;
+} Block;
+
+int main() {
+    Block blocks[MAX_BLOCKS];
+    int blockCount = 0;
+
+    FILE *fp = popen("python3 HuskyLens_ReadAndParse.py", "r");
+    if (!fp) {
+        perror("Failed to run HuskyLens_ReadAndParse.py");
+        return 1;
     }
 
-    PyObject *pName, *pModule, *pFunc;
-    PyObject *pValue, *pDict;
-    Py_ssize_t list_size, i;
-    int count = 0;
-
-    // Initialize Python interpreter
-    Py_Initialize();
-
-    // Add the directory containing the Python script to sys.path
-    PyRun_SimpleString("import sys; sys.path.insert(0, '/home/debian/Projet_Guimbarde/py files')");
-
-    // Import huskylens module
-    pName = PyUnicode_DecodeFSDefault("HuskyLens_ReadAndParse");
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-
-    if (!pModule) {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load huskylens module\n");
-        Py_Finalize();
-        return -1;
-    }
-
-    // Get the function get_parsed_huskylens_objects
-    pFunc = PyObject_GetAttrString(pModule, "get_parsed_huskylens_objects");
-    if (!pFunc || !PyCallable_Check(pFunc)) {
-        if (PyErr_Occurred()) PyErr_Print();
-        fprintf(stderr, "Cannot find function get_parsed_huskylens_objects\n");
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-        Py_Finalize();
-        return -1;
-    }
-
-    // Call the function with no arguments
-    pValue = PyObject_CallObject(pFunc, NULL);
-    if (!pValue) {
-        PyErr_Print();
-        fprintf(stderr, "Call to get_parsed_huskylens_objects failed\n");
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-        Py_Finalize();
-        return -1;
-    }
-
-    if (!PyList_Check(pValue)) {
-        fprintf(stderr, "Return value is not a list\n");
-        Py_DECREF(pValue);
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-        Py_Finalize();
-        return -1;
-    }
-
-    list_size = PyList_Size(pValue);
-    for (i = 0; i < list_size && count < max_objects; i++) {
-        pDict = PyList_GetItem(pValue, i);  // Borrowed reference
-        if (PyDict_Check(pDict)) {
-            PyObject *id_obj = PyDict_GetItemString(pDict, "id");
-            PyObject *x_obj = PyDict_GetItemString(pDict, "x");
-            PyObject *y_obj = PyDict_GetItemString(pDict, "y");
-            PyObject *w_obj = PyDict_GetItemString(pDict, "width");
-            PyObject *h_obj = PyDict_GetItemString(pDict, "height");
-
-            objs[count].id = PyLong_AsLong(id_obj);
-            objs[count].x = PyLong_AsLong(x_obj);
-            objs[count].y = PyLong_AsLong(y_obj);
-            objs[count].width = PyLong_AsLong(w_obj);
-            objs[count].height = PyLong_AsLong(h_obj);
-
-            count++;
+    char line[MAX_LINE_LEN];
+    while (fgets(line, sizeof(line), fp) && blockCount < MAX_BLOCKS) {
+        Block b;
+        if (sscanf(line, "%d %d %d %d %d", &b.x, &b.y, &b.width, &b.height, &b.ID) == 5) {
+            blocks[blockCount++] = b;
         }
     }
 
-    // Cleanup
-    Py_DECREF(pValue);
-    Py_XDECREF(pFunc);
-    Py_DECREF(pModule);
+    pclose(fp);
 
-    // Finalize Python interpreter
-    Py_Finalize();
+    // Example: print blocks
+    for (int i = 0; i < blockCount; ++i) {
+        printf("Block %d: ID=%d X=%d Y=%d Width=%d Height=%d\n",
+               i, blocks[i].ID, blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height);
+    }
 
-    return count;
+    return 0;
 }
