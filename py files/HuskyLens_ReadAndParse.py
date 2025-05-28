12407f33b2095536f472a80c60cbfc7c07de00d4
Xavi0n <xavchampoux@gmail.com>
2025-05-28 10:37:21 -0400

    test

--- py files/HuskyLens_ReadAndParse.py
+++ py files/HuskyLens_ReadAndParse.py
@@ -1,15 +1,17 @@
-from huskylib import HuskyLensLibrary
+from huskylib import HuskyLensLibrary, algorthimsByteID
 
 def main():
     huskylens = HuskyLensLibrary("I2C", 0x32)
-    huskylens.algorithm("ALGORITHM_OBJECT_RECOGNITION")
+    #huskylens.algorithm("ALGORITHM_OBJECT_RECOGNITION")
 
-    objects = huskylens.requestAll()
-    if not objects:
-        return  # No output if no objects
+    while True:
+        if huskylens.requestAll():
+            objects = huskylens.getObjects()
+            if not objects:
+                return  # No output if no objects
 
-    for obj in objects:
-        print(f"{obj.x} {obj.y} {obj.width} {obj.height} {obj.ID}")
+            for obj in objects:
+                print(f"{obj.x} {obj.y} {obj.width} {obj.height} {obj.ID}")
 
 if __name__ == "__main__":
     main()
